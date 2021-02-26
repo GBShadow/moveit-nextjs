f6f2e48f0a105a48c2920f87412e6e977e3fa091
GBShadow <gbs.shadow@gmail.com>
2021-02-26 14:04:14 -0300

    5 dia NLW

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 import { AppProps } from 'next/app';
 
-import { ChallengesProvider } from '../hooks/Challenges';
-
 import GlobalStyles from '../styles/global';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ChallengesProvider>
+    <>
       <Component {...pageProps} />
       <GlobalStyles />
-    </ChallengesProvider>
+    </>
   );
 };
 
